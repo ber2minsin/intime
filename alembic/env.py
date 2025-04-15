0f7eb18251cc88bedb44996a43f2ea42c2d0d3fe
ber2minsin <58640190+ber2minsin@users.noreply.github.com>
2025-04-15 13:42:24 +0300

    add: first tables to be able to track app sessions

--- alembic/env.py
+++ alembic/env.py
@@ -14,11 +14,10 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+# we added
+from libs.core.src.core.db.models import Base
+
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -64,9 +63,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
