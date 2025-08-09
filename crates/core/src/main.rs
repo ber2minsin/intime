5b52fb36e29b7a357a040b08a65c9b5f31f303fe
ber2minsin <58640190+ber2minsin@users.noreply.github.com>
2025-08-09 03:02:50 +0300

    chore: move to anyhow

--- crates/core/src/main.rs
+++ crates/core/src/main.rs
@@ -1,7 +1,7 @@
 use core::{db, tracker::window_processor::WindowEventProcessor};
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> anyhow::Result<()> {
     dotenv::dotenv().ok();
 
     let db_url = std::env::var("DATABASE_URL").expect("DATABASE_URL not set in environment");
